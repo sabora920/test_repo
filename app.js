984a109fe693a399c97b8526e7b223da43636d3e
Ben Parman <benparman@gmail.com>
2017-10-30 13:37:37 -0600

    Added name & age function to app.js

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ let myTeam = 'Ryan and Ben';
 
 let courseDescr = 'thinks this is fun!';
 console.log(`${myTeam} ${courseDescr}`);
+
+function whoAmI(name, age) {
+    console.log(`Hi my name is ${name} and I'm ${age) years old`);
+}
