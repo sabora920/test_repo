e035ca60aa2032d1e76dbffe2cf8632cf6463554
Ben Parman <benparman@gmail.com>
2017-10-30 13:58:47 -0600

    Made changes to functions in app.js

--- app.js
+++ app.js
@@ -5,5 +5,14 @@ let courseDescr = 'thinks this is fun!';
 console.log(`${myTeam} ${courseDescr}`);
 
 function whoAmI(name, age) {
-    console.log(`Hi my name is ${name} and I'm ${age) years old`);
+    console.log(`Hi my name is ${name} and I'm ${age} years old`);
+    console.log(`I was born in ${yearOfBirth}`);
 }
+
+function yearOfBirth(currentYear, age) {
+    return currentYear-age;
+}
+
+function myAge()
+
+whoAmI("Ben", 100);
